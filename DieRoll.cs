a6f6dd2552b30b54c274bde5739044cd721ba451
Komal <waghmarekomal21@gmail.com>
2022-09-04 00:14:44 +0530

    [Komal] Die Roll

---
+++ DieRoll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class DieRoll
+    {
+        public int playerPosition = 0;
+        Random random = new Random();
+        public int RollDie()
+        {
+            int dieCount = random.Next(1, 7);
+            return dieCount;
+        }
+    }
+}
