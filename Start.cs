ab266ca6485d5263ec3176dc83653f9aa9615c97
Komal <waghmarekomal21@gmail.com>
2022-09-04 00:11:39 +0530

    [Komal] Start_Position

---
+++ Start.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Start
+    {
+        public int playerPosition = 0;
+    }
+}
